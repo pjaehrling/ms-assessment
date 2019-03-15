8344de25d5d52d5e240d60822d7f134bb4c37047
Philipp <philipp@mycs.com>
2019-03-16 11:40:07 +1300

    feat: implement logic for list and mean endpoint

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ms-assessment
-ms backend assessment
+## How to run:
+- install dependencies: `npm install` (installs `serverless-offline`)
+- run the code: `sls offline start``
+
+## Endpoints:
+### List each variable value in the specified range
+- `/list?field={fieldname}&start={start-date}&end={end-date}`
+- `/mean?field={fieldname}&start={start-date}&end={end-date}`
--- handler.js
+++ handler.js
@@ -1,27 +1,73 @@
 'use strict';
+const fs = require('fs');
 
-const checkQueryParams = (params) => 
-    params.hasOwnProperty('name') &&
-    params.hasOwnProperty('start') && 
-    params.hasOwnProperty('end');
+// Default response in case a query param is missing
+const missingParamResponse = {
+  statusCode: 400,
+  body: JSON.stringify({
+    message: 'missing query param (required: field, start, end)',
+  })
+};
 
-module.exports.list = async (event, context) => {
+/**
+ * Check if all required query params are set 
+ * @param {Object} params 
+ */
+const checkQueryParams = (p) => 
+  p.hasOwnProperty('field') &&
+  params.hasOwnProperty('start') &&
+  params.hasOwnProperty('end');
 
+// Load weather data from file
+// Outside of handler function, to save "reading"-time in case the container is reused
+const weatherData = JSON.parse(fs.readFileSync('data.json', 'utf8'))
+
+/**
+ * Filters the data to fit a given time range and reduces it to the requested field.
+ * 
+ * @param {Array} data 
+ * @param {String} fieldName 
+ * @param {Date} startDate 
+ * @param {Date} endDate 
+ * @returns {Array} filtered to fit the provided time range and reduced to the requested field
+ */
+const getFieldValuesInRange = (data, fieldName, startDate, endDate) => {
+  return data.filter((entry) => {
+    const entryDate = new Date(entry.datetime);
+    return (entryDate >= startDate && entryDate <= endDate);
+  }).map((entry) => entry[fieldName]);
+}
+
+/**
+ * "/list" endpoint handler function
+ */
+module.exports.list = async (event) => {
+  const qp = event.queryStringParameters;
+  if (!checkQueryParams(qp)) return missingParamResponse;
+  
+  const startDate = new Date(qp.start);
+  const endDate = new Date(qp.end);
+  const result = getFieldValuesInRange(weatherData, qp.field, startDate, endDate);
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: checkQueryParams(event.queryStringParameters) ? 'list output' : 'missing query param',
-      input: event,
-    }),
+    body: JSON.stringify(result),
   };
 };
 
-module.exports.mean = async (event, context) => {
+/**
+ * "/mean" endpoint handler function
+ */
+module.exports.mean = async (event) => {
+  const qp = event.queryStringParameters;
+  if (!checkQueryParams(qp)) return missingParamResponse;
+
+  const startDate = new Date(qp.start);
+  const endDate = new Date(qp.end);
+  const filtered = getFieldValuesInRange(weatherData, qp.field, startDate, endDate);
+  const sum = filtered.reduce((total, current) => total + parseFloat(current), 0);
+  const result = sum / filtered.length;
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: checkQueryParams(event.queryStringParameters) ? 'mean average output' : 'missing query param',
-      input: event,
-    }),
+    body: JSON.stringify({ meanAverage: result })
   };
 };
